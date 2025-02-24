d6066e5ecaefee4bb0df7fb8ded4bf2f9a18596e
Cedar Mackaness <cedarmkns@gmail.com>
2025-02-23 16:44:23 -0800

    significance filtering added

--- code/network_data_shaping.R
+++ code/network_data_shaping.R
@@ -33,7 +33,7 @@ diff_17 <- diff_17 %>% select(where(~ any(length(which(. != 0)) > 1)))
 # Turn into correlation matrix
 diff_17_corr = cor_mat(diff_17, method = "pearson", alternative = "two.sided")
 diff_17_corr[is.na(diff_17_corr)] <- 0
-p_values_17 <- cor_get_pval(diff_17_corr)
+
 
 # Plot 2017 correlation matrix
 png(height=1200, width=1800, file="../2017_corr_coeff_table.png")
@@ -44,6 +44,23 @@ diff_17_corr %>%
   cor_plot(label=FALSE) 
 
 dev.off()
+
+# Drop rownames
+diff_17_corr <- diff_17_corr[, 2:length(diff_17_corr)]
+
+
+# Filter by p-value
+p_values_17 <- cor_get_pval(diff_17_corr)[, 1:length(diff_17_corr) + 1]
+p_values_17 <- apply(p_values_17, c(1,2), function(x) x > 0.05)
+sig_corrs_17 <-  diff_17_corr * (p_values_17 < 0.05)
+
+# Set diagonal to 0
+diag(sig_corrs_17) <- 0
+
+# Drop the now entirely insignificant taxa
+sig_corrs_17 <- sig_corrs_17 %>% select(where(~ any(. != 0)))
+sig_corrs_17 <- sig_corrs_17 %>% filter(rowSums(across(everything(), ~ . != 0)) > 0)
+
 
 # Do the same for 2018
 N_18_df = filter(bugs.agg, CollDate == "2018", Treatment == "N")
@@ -58,7 +75,6 @@ diff_18 <- diff_18 %>% select(where(~ any(length(which(. != 0)) > 1)))
 
 diff_18_corr = cor_mat(diff_18, method = "pearson")
 diff_18_corr[is.na(diff_18_corr)] <- 0
-p_values_18 <- cor_get_pval(diff_18_corr)
 
 # Plot 2018 correlation matrix
 png(height=1200, width=1800, file="../2018_corr_coeff_table.png")
@@ -71,9 +87,19 @@ diff_18_corr %>%
 dev.off()
 
 # Drop rownames
-diff_17_corr <- diff_17_corr[, 2:length(diff_17_corr)]
 diff_18_corr <- diff_18_corr[, 2:length(diff_18_corr)]
 
+# Filter by p-value
+p_values_18 <- cor_get_pval(diff_18_corr)[, 1:length(diff_18_corr) + 1]
+sig_corrs_18 <-  diff_18_corr * (p_values_18 < 0.05)
+
+# Set diagonal to 0
+diag(sig_corrs_18) <- 0
+
+# Drop the now entirely insignificant taxa
+sig_corrs_18 <- sig_corrs_18 %>% select(where(~ any(. != 0)))
+sig_corrs_18 <- sig_corrs_18 %>% filter(rowSums(across(everything(), ~ . != 0)) > 0)
+
 # Write out
-write_csv(diff_17_corr, file = "../data/2017_diff_corr.csv")
-write_csv(diff_18_corr, file = "../data/2018_diff_corr.csv")
+write_csv(sig_corrs_17, file = "../data/2017_diff_corr.csv")
+write_csv(sig_corrs_18, file = "../data/2018_diff_corr.csv")
--- code/undergrad_network.py
+++ code/undergrad_network.py
@@ -4,17 +4,25 @@ import matplotlib.pyplot as plt
 
 diff_17 = pd.read_csv("../data/2017_diff_corr.csv")
 diff_17[((diff_17 < 0.7) & (diff_17 > 0)) | ((diff_17 > -0.7) & (diff_17 < 0))] = 0
-diff_17_noweight = diff_17.astype(bool).astype(int)
 
 diff_18 = pd.read_csv("../data/2018_diff_corr.csv")
-diff_17[((diff_18 < 0.7) & (diff_18 > 0)) | ((diff_18 > -0.7) & (diff_18 < 0))] = 0
-diff_18_noweight = diff_18.astype(bool).astype(int)
+diff_18[((diff_18 < 0.7) & (diff_18 > 0)) | ((diff_18 > -0.7) & (diff_18 < 0))] = 0
 
-g_17 = igraph.Graph.Adjacency(diff_17_noweight, mode="undirected", loops=False)
 
-g_18 = igraph.Graph.Adjacency(diff_17_noweight, mode="undirected", loops=False)
+g_17 = igraph.Graph.Weighted_Adjacency(diff_17, mode="undirected", loops=False)
+g_17.vs["label"] = diff_17.columns
+
+
+g_18 = igraph.Graph.Weighted_Adjacency(diff_18, mode="undirected", loops=False)
+g_18.vs["label"] = diff_18.columns
 
 # Graph
+visual_style = {}
+visual_style["edge_width"] = [w * 1.5 for w in g_18.es["weight"]]
+visual_style["edge_color"] = ['red' if g_18.es[e]['weight'] > 0 else 'blue' for e in range(g_18.ecount())]
+
 fig, ax = plt.subplots()
-igraph.plot(g_18, target=ax)
+igraph.plot(g_18, target=ax, **visual_style)
+fig.savefig("../2018_network.png", bbox_inches='tight')
 plt.show()
+
